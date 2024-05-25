7306b7cd8a7d80fe3ee0bc3d3ab3c2bc6cdef9fd
PetrKavan31 <petrkavan31@gmail.com>
2024-05-25 20:42:05 +0200

    HW - integer task works, edit

--- hw_integers.py
+++ hw_integers.py
@@ -14,12 +14,14 @@ class IntegerSet:
     def min(self):
         return min(self.elements)
 
+
 def main():
-    int_set = IntegerSet([1, 2, 3, 4, 5])
+    int_set = IntegerSet([0, 1, 2, 3, 4, 5])
     print(int_set.sum())
     print(int_set.mean())
     print(int_set.max())
     print(int_set.min())
+
 
 if __name__ == "__main__":
     main()
